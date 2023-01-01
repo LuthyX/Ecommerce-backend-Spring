57637cae7e883b419b8c847e64069c3d37907f5d
LuthyX <peteradegoke81@gmail.com>
2023-01-01 22:16:21 +0100

    fixed a security bug

--- src/test/java/com/example/Ecommerce/service/JWTServiceTest.java
+++ src/test/java/com/example/Ecommerce/service/JWTServiceTest.java
@@ -1,10 +1,15 @@
 package com.example.Ecommerce.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.MissingClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.example.Ecommerce.model.AppUser;
 import com.example.Ecommerce.repository.AppUserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -15,6 +20,9 @@ public class JWTServiceTest {
 
     @Autowired
     private AppUserRepository appUserRepository;
+
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
 
     @Test
     public void testVerificationTokenNotUsableForLogin(){
@@ -28,5 +36,17 @@ public class JWTServiceTest {
         AppUser appUser = appUserRepository.findByUsername("user1name").get();
         String token = jwtService.generateJWT(appUser);
         Assertions.assertEquals(appUser.getUsername(), jwtService.getUsername(token), "AuthToken should should produce Username  = to AppUser's Username");
+    }
+
+    @Test
+    public void testJWTNotGeneratedByUs(){
+        String token = JWT.create().withClaim("USERNAME", "USER1NAME").sign(Algorithm.HMAC256("JustATestAlgoKey"));
+        Assertions.assertThrows(SignatureVerificationException.class, ()-> jwtService.getUsername(token));
+    }
+
+    @Test
+    public void testJWTCorrectlySignedNoIssuer(){
+        String token = JWT.create().withClaim("USERNAME", "USER1NAME").sign(Algorithm.HMAC256(algorithmKey));
+        Assertions.assertThrows(MissingClaimException.class, ()-> jwtService.getUsername(token));
     }
 }
